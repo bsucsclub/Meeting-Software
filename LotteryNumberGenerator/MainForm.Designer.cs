88a82741a5898c740983a3cf7cd67962310af759
Beng89 <golt1ben@live.bemidjistate.edu>
2014-09-15 10:22:36 -0500

    Created the lottery generator.

---
+++ LotteryNumberGenerator/MainForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace LotteryNumberGenerator {
+    partial class MainForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(874, 571);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(874, 571);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Generator";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.MainForm_KeyPress);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ LotteryNumberGenerator/MainForm.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LotteryNumberGenerator {
+    public partial class MainForm : Form {
+        public MainForm() {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Random number generator.
+        /// </summary>
+        static Random rGen = new Random();
+     
+        #region Lottery Bounds
+        /// <summary>
+        /// Lower bound, inclusive
+        /// </summary>
+        static int lowerBound = 1000;
+        /// <summary>
+        /// Upper bound, exclusive
+        /// </summary>
+        static int upperBound = 1200;
+        #endregion       
+    
+        #region Sequence Control
+        // Task that the lottery runs on.
+        static Task task;
+        // Task cancellation token.
+        static CancellationTokenSource cts;
+        // Delay between number updates.
+        static int delay = 25;
+
+        /// <summary>
+        /// Starts the lottery sequence.
+        /// </summary>
+        private void startSequence() {
+            delay = 25;
+            cts = new CancellationTokenSource();
+            task = new Task(sequence);
+            task.Start();
+        }
+        /// <summary>
+        /// Stops hte lottery sequence.
+        /// </summary>
+        private void stopSequence() {
+            if (task != null && task.Status == TaskStatus.Running) {
+                cts.Cancel();
+            }
+        }
+        #endregion
+
+        #region Main Sequence
+        // How many updates have been completed.
+        int updates = 0;
+        /// <summary>
+        /// The main lottery sequence.
+        /// </summary>
+        private void sequence() {
+            int n = rGen.Next(lowerBound, upperBound);
+            while (!cts.IsCancellationRequested) {
+                n = rGen.Next(lowerBound, upperBound);
+                label1.BeginInvoke(new Action(() => {
+                    label1.Text = n.ToString();
+                    if (updates > 5) {
+                        updates = 0;
+                        toggleTextColor();                       
+                    }
+                    else {
+                        updates++;
+                    }
+                }));
+                delay = rGen.Next(0, 100);
+                Thread.Sleep(delay);
+            }
+            int genStop = rGen.Next(800, 2000);
+            while (delay < genStop) {
+                n = rGen.Next(lowerBound, upperBound);
+                label1.BeginInvoke(new Action(() => {
+                    label1.Text = n.ToString();
+                    toggleTextColor();
+                }));
+                delay += rGen.Next(0, delay);
+                Thread.Sleep(delay);
+                
+            }
+            n = rGen.Next(lowerBound, upperBound);
+            label1.BeginInvoke(new Action(() => {
+                label1.ForeColor = Color.Green;
+                label1.Text = string.Format("Winning Number:\n{0}", n);
+            }));
+        }
+        #endregion
+
+        #region Full Screen
+        // Whether or not we are full screen.
+        bool isFullScreen = false;
+        // Toggles the applications full screen state.
+        private void toggleFullScreen() {
+            if (!isFullScreen) {
+                Bounds = Screen.PrimaryScreen.Bounds;
+            }
+            else {
+                int x = (Screen.PrimaryScreen.Bounds.Width - 800) / 2;
+                int y = (Screen.PrimaryScreen.Bounds.Height - 600) / 2;
+                Bounds = new Rectangle(x, y, 800, 600);
+            }
+            isFullScreen = !isFullScreen;
+        }
+        #endregion
+
+        #region Color Changing
+        // Whether or not the current text color is red.
+        bool isRed = true;
+        /// <summary>
+        /// Toggles the current text color.
+        /// </summary>
+        private void toggleTextColor() {
+            if (isRed) {
+                label1.ForeColor = Color.Red;
+            }
+            else {
+                label1.ForeColor = Color.Blue;
+            }
+            isRed = !isRed;
+        }
+        #endregion
+
+        /// <summary>
+        /// Handle key press events.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainForm_KeyPress(object sender, KeyPressEventArgs e) {
+            e.Handled = true;
+            switch (e.KeyChar) {
+                case 'b':
+                    startSequence();
+                    break;
+                case 'e':
+                    stopSequence();
+                    break;
+                case 'f':
+                    toggleFullScreen();
+                    break;
+                case 'p':
+                    stopSequence();
+                    Close();
+                    break;
+                default:
+                    e.Handled = false;
+                    break;
+            }
+        }
+    }
+}
